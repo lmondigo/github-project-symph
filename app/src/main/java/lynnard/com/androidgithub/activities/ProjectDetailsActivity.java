8a7a49a083b13a850574afef5fe0325a575c02d7
rdhdia <ruffy.heredia@gmail.com>
2016-07-25 00:22:01 +0800

    Added feature to show project details of specific user. This still lacks styling for the ProjectDetails screen

--- app/src/main/java/lynnard/com/androidgithub/activities/ProjectDetailsActivity.java
+++ app/src/main/java/lynnard/com/androidgithub/activities/ProjectDetailsActivity.java
@@ -1,9 +1,58 @@
 package lynnard.com.androidgithub.activities;
 
-/**
- * Created by Lynnard on 7/22/2016.
- */
-public class ProjectDetailsActivity {
-    //TODO when a project is clicked
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
+import lynnard.com.androidgithub.R;
+import lynnard.com.androidgithub.models.ProjectDetails;
+
+public class ProjectDetailsActivity extends AppCompatActivity {
+
+    private ProjectDetails details;
+
+    private TextView username;
+    private TextView projectTitle;
+    private TextView projectDesc;
+    private TextView repoId;
+    private TextView language;
+    private TextView forks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_details);
+
+        // Retrieve project details
+        details = (ProjectDetails) getIntent().getSerializableExtra("details");
+
+        username = (TextView) findViewById(R.id.lblUsername);
+        projectTitle = (TextView) findViewById(R.id.lblProjectTitle);
+        projectDesc = (TextView) findViewById(R.id.lblProjectDesc);
+        repoId = (TextView) findViewById(R.id.lblRepoId);
+        language = (TextView) findViewById(R.id.lblLanguage);
+        forks = (TextView) findViewById(R.id.lblForks);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        username.setText(details.getOwnerName());
+        projectTitle.setText(details.getName());
+
+        projectDesc.setText(details.getDescription());
+        if ( details.getDescription().equals("null") ) {
+            projectDesc.setText("No description available");
+        }
+
+        repoId.setText(String.valueOf(details.getId()));
+
+        language.setText(details.getLanguage());
+        if ( details.getLanguage().equals("null") ) {
+            language.setText("No language set");
+        }
+
+        forks.setText(String.valueOf(details.getForksCount()));
+    }
 }
---
+++ app/src/main/java/lynnard/com/androidgithub/models/ProjectDetails.java
@@ -0,0 +1,96 @@
+package lynnard.com.androidgithub.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.io.Serializable;
+
+/**
+ * Created by rdhdia on 24/07/2016.
+ */
+public class ProjectDetails implements Serializable {
+    private int id;
+    private String name;
+    private String ownerName;
+    private String description;
+    private String language;
+    private int forksCount;
+
+    public ProjectDetails(int id, String name, String ownerName, String description, String language, int forksCount) {
+        this.id = id;
+        this.name = name;
+        this.ownerName = ownerName;
+        this.description = description;
+        this.language = language;
+        this.forksCount = forksCount;
+    }
+
+    protected ProjectDetails(Parcel in) {
+        this.id = in.readInt();
+        this.name = in.readString();
+        this.ownerName = in.readString();
+        this.description = in.readString();
+        this.language = in.readString();
+        this.forksCount = in.readInt();
+    }
+
+    public static final Parcelable.Creator<ProjectDetails> CREATOR = new Parcelable.Creator<ProjectDetails>() {
+        @Override
+        public ProjectDetails createFromParcel(Parcel in) {
+            return new ProjectDetails(in);
+        }
+
+        @Override
+        public ProjectDetails[] newArray(int size) {
+            return new ProjectDetails[size];
+        }
+    };
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public int getForksCount() {
+        return forksCount;
+    }
+
+    public void setForksCount(int forksCount) {
+        this.forksCount = forksCount;
+    }
+}
